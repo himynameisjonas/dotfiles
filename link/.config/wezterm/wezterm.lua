37aaccd85219de4fd794f5127d408d5dec4764c2
Jonas Brusman <jonas@brusman.se>
2024-08-13 13:12:39 +0200

    Basic WezTerm config

---
+++ link/.config/wezterm/wezterm.lua
@@ -0,0 +1,43 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.color_scheme = 'catppuccin-latte'
+config.font = wezterm.font({ family = 'Operator Mono Lig' })
+config.font_size = 17
+
+config.window_decorations = 'RESIZE'
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = true
+
+config.window_frame = {
+    font = wezterm.font({ family = 'Operator Mono Lig', weight = 'Bold' }),
+    font_size = 13,
+}
+
+wezterm.on('update-status', function(window)
+    -- Grab the utf8 character for the "powerline" left facing
+    -- solid arrow.
+    local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+
+    -- Grab the current window's configuration, and from it the
+    -- palette (this is the combination of your chosen colour scheme
+    -- including any overrides).
+    local color_scheme = window:effective_config().resolved_palette
+    local bg = color_scheme.background
+    local fg = color_scheme.foreground
+
+    window:set_right_status(wezterm.format({
+        -- First, we draw the arrow...
+        { Background = { Color = 'none' } },
+        { Foreground = { Color = bg } },
+        { Text = SOLID_LEFT_ARROW },
+        -- Then we draw our text
+        { Background = { Color = bg } },
+        { Foreground = { Color = fg } },
+        { Text = ' ' .. wezterm.hostname() .. ' ' },
+    }))
+end)
+
+
+-- Returns our config to be evaluated. We must always do this at the bottom of this file
+return config
